0f8d11d883c5f002b87065e0bfee67b151fb848a
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2023-12-22 20:27:43 -0400

    fix

--- src/langs/handler.ts
+++ src/langs/handler.ts
@@ -5,49 +5,50 @@ import { PotoHandler } from "../utils";
 const values = Object.values(Locale);
 
 export class PotoLangsHandler extends PotoHandler {
-    record: Partial<Record<LocaleString, any>> = {};
-    protected filter = (path: string) => path.endsWith(".json");
+	record: Partial<Record<LocaleString, any>> = {};
+	protected filter = (path: string) => path.endsWith(".json");
 	defaultLang?: LocaleString
 
-    getKey(lang: string, message: string) {
-        let value: any = this.record[lang as LocaleString];
+	getKey(lang: string, message: string) {
+		let value: any = this.record[lang as LocaleString];
 
-        for (const i of message.split(".")) {
-            value = value[i];
-        }
+		for (const i of message.split(".")) {
+			value = value[i];
+		}
 
-        if (typeof value !== "string") {
-            return undefined;
-        }
+		if (typeof value !== "string") {
+			return undefined;
+		}
 
-        return value;
-    }
+		return value;
+	}
 
-    parse(str: string, metadata: Record<string, any>) {
-        const regex = /{{(.*?)}}/g;
-        return str.replace(regex, (match) => metadata[match.slice(2, -2)] ?? match);
-    }
+	parse(str: string, metadata: Record<string, any>) {
+		const regex = /{{(.*?)}}/g;
+		return str.replace(regex, (match) => metadata[match.slice(2, -2)] ?? match);
+	}
 
-    get<K extends keyof __LangType>(lang: LocaleString, message: K, metadata: __LangType[K]) {
-        try {
-        	const value = this.getKey(lang, message) ?? (this.defaultLang ? this.getKey(this.defaultLang!, message) : undefined)
-            if (!value) {
-                throw new Error("Invalid key");
-            }
-            return this.parse(value, metadata);
-        } catch {
-            throw new Error("Invalid key");
-        }
-    }
+	get<K extends keyof __LangType>(lang: LocaleString, message: K, metadata: __LangType[K]): string {
+		try {
+			const value = this.getKey(lang, message) ?? (this.defaultLang ? this.getKey(this.defaultLang!, message) : undefined)
+			if (!value) {
+				throw new Error("Invalid key");
+			}
+			return this.parse(value, metadata);
+		} catch {
+			if (lang === this.defaultLang || !this.defaultLang) throw new Error("Invalid key");
+			return this.get(this.defaultLang, message, metadata);
+		}
+	}
 
-    async load(dir: string) {
-        const files = await this.loadFilesK<Record<string, any>>(await this.getFiles(dir));
-        for (const i of files) {
-            if (!values.includes(i.name.slice(0, -5) as any)) {
-                this.logger.fatal(`Invalid lang [${i.name.slice(0, -5)}]`);
-                continue;
-            }
-            this.record[i.name.slice(0, -5) as LocaleString] = i.file;
-        }
-    }
+	async load(dir: string) {
+		const files = await this.loadFilesK<Record<string, any>>(await this.getFiles(dir));
+		for (const i of files) {
+			if (!values.includes(i.name.slice(0, -5) as any)) {
+				this.logger.fatal(`Invalid lang [${i.name.slice(0, -5)}]`);
+				continue;
+			}
+			this.record[i.name.slice(0, -5) as LocaleString] = i.file;
+		}
+	}
 }
