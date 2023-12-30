71d60f76aaea88953cf694ad7b5b80a8d33999e8
socram03 <marcosjgs03@gmail.com>
2023-12-30 13:52:45 -0400

    x

--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from 'discord-api-types/v10';
-export * from '../common/types/options';
-export * from '../common/types/resolvables';
-export * from '../common/types/util';
-export * from '../common/types/write';
-
