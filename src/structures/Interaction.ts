04fdd6bbe49c7a82bd5a664837fcfe96e0d299ea
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2024-01-01 14:10:28 -0400

    fix

--- src/structures/Interaction.ts
+++ src/structures/Interaction.ts
@@ -59,9 +59,9 @@ import { BaseChannel } from './methods/channel/base';
 export type ReplyInteractionBody =
 	| { type: InteractionResponseType.Modal; data: ModalCreateBodyRequest }
 	| {
-		type: InteractionResponseType.ChannelMessageWithSource | InteractionResponseType.UpdateMessage;
-		data: InteractionCreateBodyRequest | InteractionMessageUpdateBodyRequest | ComponentInteractionMessageUpdate;
-	}
+			type: InteractionResponseType.ChannelMessageWithSource | InteractionResponseType.UpdateMessage;
+			data: InteractionCreateBodyRequest | InteractionMessageUpdateBodyRequest | ComponentInteractionMessageUpdate;
+	  }
 	| Exclude<RESTPostAPIInteractionCallbackJSONBody, APIInteractionResponsePong>;
 
 /** @internal */
@@ -70,7 +70,7 @@ export type __InternalReplyFunction = (_: { body: APIInteractionResponse; files?
 export interface BaseInteraction
 	extends ObjectToLower<
 		Omit<APIBaseInteraction<InteractionType, any>, 'user' | 'member' | 'message' | 'channel' | 'type'>
-	> { }
+	> {}
 
 export class BaseInteraction<
 	FromGuild extends boolean = boolean,
@@ -125,15 +125,15 @@ export class BaseInteraction<
 						body.data instanceof Modal
 							? body.data.toJSON()
 							: {
-								...body.data,
-								components: body.data?.components
-									? body.data.components.map((x) =>
-										x instanceof ActionRow
-											? (x.toJSON() as unknown as APIActionRowComponent<APITextInputComponent>)
-											: x,
-									)
-									: [],
-							},
+									...body.data,
+									components: body.data?.components
+										? body.data.components.map((x) =>
+												x instanceof ActionRow
+													? (x.toJSON() as unknown as APIActionRowComponent<APITextInputComponent>)
+													: x,
+										  )
+										: [],
+							  },
 				};
 			default:
 				return body;
@@ -167,7 +167,11 @@ export class BaseInteraction<
 		await this.replied;
 
 		this.client.components.onRequestInteraction(
-			body.type === InteractionResponseType.Modal ? this.user.id : body.type === InteractionResponseType.UpdateMessage ? this.message!.interaction!.id : this.id,
+			body.type === InteractionResponseType.Modal
+				? this.user.id
+				: body.type === InteractionResponseType.UpdateMessage
+				  ? this.message!.interaction!.id
+				  : this.id,
 			body,
 		);
 	}
@@ -248,7 +252,7 @@ export type PotoInteraction =
 export interface AutocompleteInteraction
 	extends ObjectToLower<
 		Omit<APIApplicationCommandAutocompleteInteraction, 'user' | 'member' | 'type' | 'data' | 'message' | 'channel'>
-	> { }
+	> {}
 
 export class AutocompleteInteraction<FromGuild extends boolean = boolean> extends BaseInteraction<
 	FromGuild,
@@ -393,7 +397,7 @@ export class ApplicationCommandInteraction<
 export interface ComponentInteraction
 	extends ObjectToLower<
 		Omit<APIMessageComponentInteraction, 'user' | 'member' | 'type' | 'data' | 'message' | 'channel'>
-	> { }
+	> {}
 
 export class ComponentInteraction<
 	FromGuild extends boolean = boolean,
@@ -507,14 +511,14 @@ export class MentionableSelectMenuInteraction extends SelectMenuInteraction {
 			: [];
 		this.members = resolved.members
 			? this.values.map(
-				(x) =>
-					new InteractionGuildMember(
-						this.client,
-						resolved.members![x],
-						this.users!.find((u) => u.id === x)!,
-						this.guildId!,
-					),
-			)
+					(x) =>
+						new InteractionGuildMember(
+							this.client,
+							resolved.members![x],
+							this.users!.find((u) => u.id === x)!,
+							this.guildId!,
+						),
+			  )
 			: [];
 		this.users = resolved.users ? this.values.map((x) => new User(this.client, resolved.users![x])) : [];
 	}
@@ -546,14 +550,14 @@ export class UserSelectMenuInteraction extends SelectMenuInteraction {
 		this.users = this.values.map((x) => new User(this.client, resolved.users[x]));
 		this.members = resolved.members
 			? this.values.map(
-				(x) =>
-					new InteractionGuildMember(
-						this.client,
-						resolved.members![x],
-						this.users!.find((u) => u.id === x)!,
-						this.guildId!,
-					),
-			)
+					(x) =>
+						new InteractionGuildMember(
+							this.client,
+							resolved.members![x],
+							this.users!.find((u) => u.id === x)!,
+							this.guildId!,
+						),
+			  )
 			: [];
 	}
 }
