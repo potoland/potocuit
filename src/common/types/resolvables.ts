e9f4c7f63551f7d040f5310973323e6575467b06
socram03 <marcosjgs03@gmail.com>
2024-01-09 17:00:01 -0400

    fix ColorResolvable autocomplete

--- src/common/types/resolvables.ts
+++ src/common/types/resolvables.ts
@@ -3,4 +3,4 @@ import type { GuildMember } from '../../structures';
 
 export type EmojiResolvable = string | Partial<APIPartialEmoji> | `<${string | undefined}:${string}:${string}>`;
 export type GuildMemberResolvable = string | Partial<GuildMember>;
-export type ColorResolvable = string | number | keyof typeof EmbedColors | 'Random' | [number, number, number];
+export type ColorResolvable = `#${string}` | number | keyof typeof EmbedColors | 'Random' | [number, number, number];
