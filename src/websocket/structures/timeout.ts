effda1a645ee2bf03e213f572d9301dde42af11f
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2024-01-09 21:52:17 -0400

    fix connectTimeout

--- src/websocket/structures/timeout.ts
+++ src/websocket/structures/timeout.ts
@@ -1,7 +1,7 @@
 export class ConnectTimeout {
 	promises: { promise: Promise<boolean>; resolve: (x: boolean) => any }[] = [];
 	interval?: NodeJS.Timeout = undefined;
-	constructor(readonly intervalTime = 5000) {}
+	constructor(readonly intervalTime = 5000) { }
 
 	wait() {
 		let resolve = (_x: boolean) => {
@@ -12,6 +12,7 @@ export class ConnectTimeout {
 			this.interval = setInterval(() => {
 				this.shift();
 			}, this.intervalTime);
+			resolve(true)
 		}
 		this.promises.push({ resolve, promise });
 		return promise;
