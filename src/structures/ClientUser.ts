051c47fc06d066868cb5c6dc94812110b2202cec
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2024-01-14 20:39:30 -0400

    dont get from cache

--- src/structures/ClientUser.ts
+++ src/structures/ClientUser.ts
@@ -43,7 +43,7 @@ export class ClientUser extends User {
 			fetch: async (id: string) => {
 				const guild = await ctx.client.proxy.guilds(id).get();
 				await ctx.client.cache.guilds?.patch(id, guild);
-				return (await ctx.client.cache.guilds?.get(id)) ?? new Guild(ctx.client, guild);
+				return new Guild(ctx.client, guild);
 			},
 			fetchSelf: async (id: string) => {
 				const self = await ctx.client.proxy.users('@me').guilds(id).member.get();
