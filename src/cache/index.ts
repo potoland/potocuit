d9d9b65520045357bb4b4eef4b410eb1d9305e2d
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2024-03-06 16:33:26 -0400

    -

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -193,19 +193,19 @@ export class Cache {
 	async bulkGet(
 		keys: (
 			| readonly [
-				/* type */
-				NonGuildBased | GuildRelated,
-				/* source id */
-				string,
-			]
+					/* type */
+					NonGuildBased | GuildRelated,
+					/* source id */
+					string,
+			  ]
 			| readonly [
-				/* type */
-				GuildBased,
-				/* source id */
-				string,
-				/* guild id */
-				string,
-			]
+					/* type */
+					GuildBased,
+					/* source id */
+					string,
+					/* guild id */
+					string,
+			  ]
 		)[],
 	) {
 		const allData: Partial<Record<NonGuildBased | GuildBased | GuildRelated, string[][]>> = {};
@@ -264,23 +264,23 @@ export class Cache {
 	async bulkPatch(
 		keys: (
 			| readonly [
-				/* type */
-				NonGuildBased,
-				/* data */
-				any,
-				/* source id */
-				string,
-			]
+					/* type */
+					NonGuildBased,
+					/* data */
+					any,
+					/* source id */
+					string,
+			  ]
 			| readonly [
-				/* type */
-				GuildBased | GuildRelated,
-				/* data */
-				any,
-				/* source id */
-				string,
-				/* guild id */
-				string,
-			]
+					/* type */
+					GuildBased | GuildRelated,
+					/* data */
+					any,
+					/* source id */
+					string,
+					/* guild id */
+					string,
+			  ]
 		)[],
 	) {
 		const allData: [string, any][] = [];
@@ -348,23 +348,23 @@ export class Cache {
 	async bulkSet(
 		keys: (
 			| readonly [
-				/* type */
-				NonGuildBased,
-				/* data */
-				any,
-				/* source id */
-				string,
-			]
+					/* type */
+					NonGuildBased,
+					/* data */
+					any,
+					/* source id */
+					string,
+			  ]
 			| readonly [
-				/* type */
-				GuildBased | GuildRelated,
-				/* data */
-				any,
-				/* source id */
-				string,
-				/* guild id */
-				string,
-			]
+					/* type */
+					GuildBased | GuildRelated,
+					/* data */
+					any,
+					/* source id */
+					string,
+					/* guild id */
+					string,
+			  ]
 		)[],
 	) {
 		const allData: [string, any][] = [];
